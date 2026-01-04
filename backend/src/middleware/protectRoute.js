6946800cb96324dbc0195a189cc5785858704001
kumarmanoj231 <lodhamanoj383@gmail.com>
2026-01-04 19:36:50 +0530

    minor typo fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,17 +6,17 @@ export const protectRoute = [
     async(req, res, next)=>{
         try {
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized - invalid token"});
+            if(!clerkId) return res.status(401).json({message: "Unauthorized - invalid token"});
 
             const user = await User.findOne({clerkId})
 
-            if(!User) return res.status(404).json({msg: "User not found"});
+            if(!User) return res.status(404).json({message: "User not found"});
 
             req.user = user;
             next();
 
         } catch (error) {
-            console.error("Error in protect route middleware", error);
+            console.error("Error in protectRoute middleware", error);
             res.status(500).json({message: "Internal server error"});
         }
     }
